d87d2a2aba6af026ab83c6187861b2108c95da4e
Thyago Erick <thyago123gois@gmail.com>
2023-04-14 18:30:28 -0300

    feat: Criação do componente InputPesquisa e style

---
+++ frontend/src/components/NavbarLogin/InputPesquisa.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+import "./styles/InputPesquisa.css"
+
+const InputPesquisa = () => {
+    return (
+        <>
+            <div className="p-inputgroup container">
+                <InputText placeholder="Pesquise pelo título desejado" className="input-pesquisa"/>
+                <Button icon="pi pi-search" className="button-pesquisa"/>
+            </div>
+        </>      
+    )
+}
+
+export default InputPesquisa;
