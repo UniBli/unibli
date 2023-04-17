7ebc9bbdb9c555477444f4c33f81b2d7b78dde20
Thyago Erick <thyago123gois@gmail.com>
2023-04-17 19:34:37 -0300

    feat: Adicionando componente Avatar e style

--- frontend/src/components/Auth0/Profile.js
+++ frontend/src/components/Auth0/Profile.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
+import { Avatar } from 'primereact/avatar';
+// import { Container } from 'react-bootstrap'; NÃO ESTOU CONSEGUINDO USAR 
+
+import './styles/Profile.css'
+
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
@@ -10,11 +15,13 @@ const Profile = () => {
 
   return (
     isAuthenticated && (
-      <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
+      <>
+        <div className="containerProfile">
+        <Avatar image={user.picture} alt={user.name} size="xlarge" shape="circle"/>
+        <h3>{user.name}</h3>
         <p>{user.email}</p>
-      </div>
+        </div>
+      </>
     )
   );
 };
