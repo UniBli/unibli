083dfa1dd6f1c39366df9f30c65f46a953701716
Thyago Erick <thyago123gois@gmail.com>
2023-04-21 02:39:39 -0300

    feat: Adicionando texto Menu no header do Slidebar

--- frontend/src/components/Auth0/Profile.js
+++ frontend/src/components/Auth0/Profile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 import { Avatar } from 'primereact/avatar';
@@ -7,6 +7,22 @@ import { Avatar } from 'primereact/avatar';
 import './styles/Profile.css'
 
 const Profile = () => {
+
+  //Para adicionar a palavra 'Menu' no header do componente Sidebar 
+  useEffect(() => {
+    const sidebarHeader = document.querySelector(".p-sidebar-header");
+    const txtMenu = document.createElement("h2");
+    
+    txtMenu.textContent = "Menu";
+    txtMenu.style.color = "#055904";
+    txtMenu.style.margin = "0";
+    
+    //Verificação para escrever o txtMenu apenas 1 vez
+    if (!sidebarHeader.querySelector("h2")) {
+      sidebarHeader.insertBefore  (txtMenu, sidebarHeader.firstChild);
+    }
+  }, []);
+
   const { user, isAuthenticated, isLoading } = useAuth0();
 
   if (isLoading) {
