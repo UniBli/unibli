16faf610243b18ebf31ad86aab95f23fdd1ea172
Thyago Erick <thyago123gois@gmail.com>
2023-06-09 04:19:31 -0300

    feat: Criando o componente ConsultarTitulos

---
+++ frontend/src/components/ConsultarTitulos/ConsultarTitulos.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles/ConsultarTitulos.css';
+
+function ConsultarTitulos() {
+  return (
+    <section className='consultarTitulos'>
+        <div className='imgBanner'>
+            <p>O sistema <span>UniBli</span> veio</p>
+            <p>para facilitar a vida do</p> 
+            <p>leitor Fatecano, que</p> 
+            <p>mora longe da sua</p> 
+            <p>unidade!</p>
+        </div>
+    </section>
+  );
+}
+
+export default ConsultarTitulos;
