0a7801d295b606a7035428b689d994c598c33373
Thyago Erick <thyago123gois@gmail.com>
2023-06-09 04:24:52 -0300

    feat: Adicionando fontes Montserrat e Roboto

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,10 @@ import { Auth0Provider } from "@auth0/auth0-react"; //serviço de autenticação
 import "primereact/resources/themes/bootstrap4-light-blue/theme.css";//theme    
 import "primereact/resources/primereact.min.css";//core
 import "primeicons/primeicons.css";//icons                                      
-        
+
+import { Scrollbars } from 'react-custom-scrollbars';
+
+
 //variaveis de ambiente configuradas no .env
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
@@ -20,7 +23,18 @@ root.render(
         authorizationParams={{
           redirect_uri: window.location.origin
       }}>
-        <App/>
+      <Scrollbars
+          autoHide
+          autoHideTimeout={1000}
+          autoHideDuration={200}
+          thumbSize={400}
+          style={{ width: '100%', height: '100vh' }}
+          renderThumbVertical={props => (
+            <div {...props} style={{ backgroundColor: '#055904', borderRadius:'5px' }} />
+          )}
+        >
+          <App/>
+        </Scrollbars>
       </Auth0Provider>
   </React.StrictMode>
 );
