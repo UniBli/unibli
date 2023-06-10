8adf769aad41b7a56a2019c352eaf03bc44570bb
Thyago Erick <thyago123gois@gmail.com>
2023-06-10 14:27:28 -0300

    feat: Criação do componente Footer

--- frontend/src/components/FooterPage/FooterPage.js
+++ frontend/src/components/FooterPage/FooterPage.js
@@ -1,8 +1,35 @@
 import React from "react";
+import "./styles/FooterPage.css"
+import logoUniBli from "../../assets/img/logoUniBli+Texto_F-Negativo.svg"
 
 const FooterPage = () => {
   return (
     <>
+    <section id="footer">
+
+      <div className="logoUniBli">
+        <img
+          src={logoUniBli}
+          className="d-inline-block align-top"
+          alt="Logo UniBli"
+        />
+        <small>
+          
+        Todos os direitos reservados a ©UniBli
+        </small>
+      </div>
+
+        <small>
+          Feito com 💚 por, Thyago, Weslley e Hélio
+        </small>
+
+
+        <small>
+          Orientador  Rodrigo Bossini
+        </small> 
+
+
+    </section>
     </>
   );
 };
